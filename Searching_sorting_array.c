6fa6e4e5a4273a7ff65b582e2109d42bdbe497ae
rohit <rohitk54676@gmail.com>
2025-11-14 21:29:29 +0530

    added Searching

--- Searching_sorting_array.c
+++ Searching_sorting_array.c
@@ -53,17 +53,62 @@ void SelectionSort(int arr[],int n){
         printf("%d ",arr[i]);
     }
 }
+void LinearSearch(int arr[],int n,int num){
+    int index=-1;
+    for(int i=0;i<n;i++){
+        if(arr[i]==num){
+            index=i+1;
+            break;
+        }
+    }
+    if(index==-1){
+        printf("\n Not Found \n");
+    }else{
+        printf("Found at position %d",index);
+    }
+}
+void BinarySearch(int arr[],int n,int num){
+    int index=-1;
+    int low=0;
+    int high=n-1;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(arr[mid]==num){
+            index=mid+1;
+            break;
+        }
+        if(arr[mid]>num){
+            high=mid-1;
+        }
+        if(arr[mid]<num){
+            low=mid+1;
+        }
+    }
+    if(index==-1){
+        printf("\n Not Found \n");
+    }else{
+        printf("Found at position %d",index);
+    }
+}
 int main(){
     printf("Enter no of element in array");
     int a;
     scanf("%d",&a);
     int arr[a];
+    int arr1[a];
     for(int i=0;i<a;i++){
         printf("Now enter elemets %d ",i+1);
         scanf("%d",&arr[i]);
+        arr1[i]=arr[i];
     }
     Display(arr,a);
     BubbleSort(arr,a);
     InsertionSort(arr,a);
     SelectionSort(arr,a);
+    int find;
+    printf("Enter the element to find");
+    scanf("%d",&find);
+    LinearSearch(arr1,a,find);
+    BinarySearch(arr,a,find);
+
 }
